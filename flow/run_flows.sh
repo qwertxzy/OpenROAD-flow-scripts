192f4e33777a84b4e39e1e20fb79e907de883692
qwertxzy <leonard.pudwitz@stud.tu-darmstadt.de>
2025-06-13 22:54:28 +0200

    Adding arg check to run script

--- flow/run_flows.sh
+++ flow/run_flows.sh
@@ -3,6 +3,11 @@
 # Set design dir here
 DESIGN_DIR=$1
 
+if [ $# -eq 0 ]; then
+  echo "Provide a path to a design dir"
+  exit 1
+fi
+
 echo "Running design from $DESIGN_DIR"
 
 # Run TritonMP flow
